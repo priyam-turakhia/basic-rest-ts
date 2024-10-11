e6ca00bd851cdd70ad6c742af115ead7d132619f
4RCH4NG3L69 <94463402+4RCH4NG3L69@users.noreply.github.com>
2024-10-11 07:26:52 +0200

    Update index.ts

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto';
 
-const SECRET = 'PRIYAM-REST-API';
+const SECRET = 'github';
 
 export const authentication = (salt: string, password: string): string => {
   return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
